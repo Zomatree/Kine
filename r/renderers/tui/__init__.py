13225ea69d6cd8c2ff5de476963fb8d43f099873
Zomatree <me@zomatree.live>
2022-11-17 16:49:06 +0000

    more tui stuff

--- r/renderers/tui/__init__.py
+++ r/renderers/tui/__init__.py
@@ -1,7 +1,5 @@
-import asyncio
 from asyncio import events
-from typing import ParamSpec
-from r.core import VNode
+from typing import ParamSpec, cast
 
 from r.utils import ElementId
 
@@ -13,6 +11,7 @@ from textual.app import App as BaseApp, ComposeResult
 from textual.containers import Container
 from textual.widget import Widget
 from textual.widgets import *
+from textual.message import Message as TextualMessage
 from textual import events
 
 from .elements import *
@@ -23,20 +22,26 @@ class App(BaseApp[None]):
     def __init__(self, dom: VirtualDom) -> None:
         super().__init__()
         self.nodes: dict[ElementId, Widget | str] = {}
+        self.element_ids: dict[Widget, ElementId] = {}
+
         self.dom = dom
 
-    def on_mount(self, event: events.Mount):
-        self.nodes[ElementId(0)] = self.query_one("#main")
+    async def on_mount(self, event: events.Mount):
+        id = ElementId(0)
+        self.nodes[id] = main = self.query_one("#main")
+        self.element_ids[main] = id
+
         mutations = self.dom.rebuild()
 
-        self.handle_mutations(mutations)
+        await self.handle_mutations(mutations)
 
-    def handle_mutations(self, mutations: Mutations):
+    async def handle_mutations(self, mutations: Mutations):
         for mod in mutations.modifications:
+            print(mod)
             match mod:
                 case diff.CreateElement():
-                    widget = self.create_widget(mod.tag)
-                    self.nodes[mod.root] = widget
+                    self.nodes[mod.root] = widget = self.create_widget(mod.tag)
+                    self.element_ids[widget] = mod.root
 
                 case diff.CreateTextNode():
                     self.nodes[mod.root] = mod.text
@@ -44,10 +49,10 @@ class App(BaseApp[None]):
                 case diff.AppendChildren():
                     parent = self.nodes[mod.root]
                     assert not isinstance(parent, str)
+                    children: list[Widget] = []
 
                     for child_id in mod.children:
                         child = self.nodes[child_id]
-                        print(parent, child)
                         if isinstance(child, str):
                             if isinstance(parent, Button):
                                 parent.label = child
@@ -55,18 +60,20 @@ class App(BaseApp[None]):
                                 parent.renderable = child
                                 assert isinstance(parent, Static)
                         else:
-                            parent._add_child(child)
+                            children.append(child)
 
-                    self.refresh()
+                    await parent.mount(*children)
 
                 case diff.NewEventListener():
                     node = self.nodes[mod.root]
+                    assert isinstance(node, Widget)
 
-                    async def event_callback(event, *, event_name: str = mod.event_name):
-                        node.label = "+2"
-                        getattr(type(node), f"_on_{event_name}")(node, event)
+                    def press(event: TextualMessage, *, node: Widget = node, event_name: str = mod.event_name):
+                        element_id = self.element_ids[cast(Widget, event.sender)]
+                        self.dom.handle_message(messages.EventMessage(None, 0, element_id, event_name, True, event))
 
-                    setattr(node, f"_on_{mod.event_name}", event_callback)
+                    setattr(node, f"_on_button_{mod.event_name}", event_handler)
+
                 case _:
                     raise Exception(str(mod))
 
@@ -78,6 +85,8 @@ class App(BaseApp[None]):
                 return Static()
             case "button":
                 return Button(variant="success")
+            case "input":
+                return Input()
             case "header":
                 return Header()
             case "footer":
