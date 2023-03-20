69e5fdaab9a0bf53b56669a3d7a8c0c3d8c5bb74
Zomatree <me@zomatree.live>
2023-03-20 22:35:32 +0000

    add built-in local storage hooks

--- kine/ext/wasm/__init__.py
+++ kine/ext/wasm/__init__.py
@@ -1,6 +1,7 @@
+from spec import TypeVar
 from ... import component, Scope
 from ...renderers.web_elements import *
-from typing import ParamSpec
+from typing import ParamSpec, overload
 import js
 import pyodide.ffi
 
@@ -94,3 +95,64 @@ def router(cx: Scope):
     cx.use_hook(lambda: cx.provide_context(UseRouter(cx)))
 
     return cx.render(div[cx.children])
+
+T = TypeVar("T")
+
+class Storage:
+    def __init__(self, cx: Scope, backing: js.Storage):
+        self.cx = cx
+        self.backing = backing
+
+    @overload
+    def get(self, name: str, default: T) -> str | T:
+        ...
+
+    @overload
+    def get(self, name: str, default: None = ...) -> str | None:
+        ...
+
+    def get(self, name: str, default: T | None = None) -> str | None | T:
+        return self.backing.getItem(name) or default
+
+    def set(self, name: str, value: str):
+        self.backing.setItem(name, value)
+        self.cx.schedule_update()
+
+    def remove(self, name: str):
+        self.backing.removeItem(name)
+        self.cx.schedule_update()
+
+    def clear(self):
+        self.backing.clear()
+        self.cx.schedule_update()
+
+    @overload
+    def key(self, index: int, default: T) -> str | T:
+        ...
+
+    @overload
+    def key(self, index: int, default: None = ...) -> str | None:
+        ...
+
+    def key(self, index: int, default: T | None = None) -> str | None | T:
+        return self.backing.key(index) or default
+
+    def __len__(self) -> int:
+        return self.backing.length
+
+    def __getitem__(self, key: str) -> str:
+        value = self.get(key)
+
+        if value is None:
+            raise IndexError
+
+        return value
+
+    def __setitem__(self, key: str, value: str):
+        self.set(key, value)
+
+def use_local_storage(cx: Scope) -> Storage:
+    return cx.use_hook(lambda: Storage(cx, js.window.localStorage))
+
+def use_session_storage(cx: Scope) -> Storage:
+    return cx.use_hook(lambda: Storage(cx, js.window.sessionStorage))
